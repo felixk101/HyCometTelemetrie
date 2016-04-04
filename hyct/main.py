3c8591cf91353f414384922aa711fa2cffcf6911
lukasedl <LukasPhilipp.Edlboeck@hs-augburg.de>
2016-04-04 17:19:34 +0200

    main added and receiver file created

--- hyct/main.py
+++ hyct/main.py
@@ -1,1 +1,20 @@
-1+1
+from receiver import start_receiver
+from multiprocessing import Pool
+
+def dummy_function():
+    pass
+
+if __name__ == '__main__':
+    print('HyComent Telemetry is starting')
+    print('Creating new processes')
+    pool = Pool()
+    receiver_process = pool.apply_async(start_receiver)
+    dummy_process=pool.apply_async(dummy_function)
+    try:
+        receiver_answer = receiver_process.get()
+    except:
+        print('ERROR')
+    try:
+        dummy_answer = dummy_process.get()
+    except:
+        print('ERROR')
