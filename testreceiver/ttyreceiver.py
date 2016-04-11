275fcdd137170e4a389b27d77c57ab5b3be2a8fa
Felix Kampfer <felixk101@gmail.com>
2016-04-11 03:44:43 +0200

    ttyreceiver now ready raw bytes

--- testreceiver/ttyreceiver.py
+++ testreceiver/ttyreceiver.py
@@ -1,20 +1,23 @@
+# -*- coding: utf-8 -*-
 import serial;
 import binascii;
+from string import join;
 
 ser = serial.Serial(
     port='/dev/ttyUSB0',
     baudrate=115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
+    bytesize=serial.EIGHTBITS
 )
+print('reading 10 bytes at a time:')
+
 while(True):
 
+    print('--------------------')
+    line = ser.read(10)
+    print([x for x in line])
+    print(join([x for x in line]))
+    print(line.encode('hex'))
+    
 
-    print('waiting')
-    line = ser.readline()
-    print(line)
-
-    #binary = binascii.unhexlify(b'TELEM 22570954020800f207207401002f0bbbff320840ff8cff1200d2ffc50037ffa601f885a0')
-    #print(binary)
