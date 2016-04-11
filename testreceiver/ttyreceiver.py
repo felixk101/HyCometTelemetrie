3414017958cce2ee8aa0bf36d6b27be3ee4d25fa
Felix Kampfer <felixk101@gmail.com>
2016-04-11 02:34:02 +0200

    successfully read from ttl-connected raspi

--- testreceiver/ttyreceiver.py
+++ testreceiver/ttyreceiver.py
@@ -1,15 +1,20 @@
 import serial;
+import binascii;
 
 ser = serial.Serial(
-    port='/dev/pts/10',
-    baudrate=9600,
-    parity=serial.PARITY_ODD,
-    stopbits=serial.STOPBITS_TWO,
-    bytesize=serial.SEVENBITS,
-    rtscts=True,
-    dsrdtr=True
+    port='/dev/ttyUSB0',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
 )
 while(True):
+
+
     print('waiting')
     line = ser.readline()
     print(line)
+
+    #binary = binascii.unhexlify(b'TELEM 22570954020800f207207401002f0bbbff320840ff8cff1200d2ffc50037ffa601f885a0')
+    #print(binary)
